75cbd1ea04fe4a8f170114bf1838cedb968e6a92
bonnieli1414 <87414209+bonnieli1414@users.noreply.github.com>
2025-09-05 00:24:53 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue";
+const num = ref(2);
+function clickMe() {
+  num.value++;
+}
+</script>
+
+<template>
+  <h1>練習2025/09/04</h1>
+  {{ num }}
+  <button type="button" v-on:click="clickMe">點我</button>
+</template>
